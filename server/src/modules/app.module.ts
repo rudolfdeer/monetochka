9eb23017a704b8475143730d71dbe765a6180c75
rudolfdeer <tomalichko@gmail.com>
2022-09-05 17:26:25 +0300

    mon-25: configure .env file with db connection credentials

--- server/src/modules/app.module.ts
+++ server/src/modules/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     MongooseModule.forRoot(
-      'mongodb+srv://admin:admin@cluster0.az6eu.mongodb.net/?retryWrites=true&w=majority',
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.az6eu.mongodb.net/?retryWrites=true&w=majority`,
       {
         dbName: 'monetochka',
       },
